e8dd096e2e3e8ae1110bcddeb2d9f63fe88d0ad2
Vippauttej <106222774+Vippauttej@users.noreply.github.com>
2025-04-08 00:07:36 +0530

    Update OrangeHRMTest.java

--- src/com/orangehrm/tests/OrangeHRMTest.java
+++ src/com/orangehrm/tests/OrangeHRMTest.java
@@ -1,8 +1,11 @@
 package com.orangehrm.tests;
 
 import com.orangehrm.pages.*;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,12 +18,13 @@ public class OrangeHRMTest {
     LoginPage loginPage;
     DashboardPage dashboardPage;
     PIMPage pimPage;
+    
 
     @BeforeClass
     public void setup() {
 //        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
@@ -40,7 +44,7 @@ public class OrangeHRMTest {
     @Test(priority = 2)
     public void addEmployeesTest() {
         dashboardPage.navigateToPIM();
-        pimPage.addEmployee("sai", "ss");
+        pimPage.addEmployee("Sai", "ss");
         dashboardPage.navigateToPIM();
         pimPage.addEmployee("shailu", "sm");
         dashboardPage.navigateToPIM();
@@ -52,13 +56,21 @@ public class OrangeHRMTest {
     @Test(priority = 3)
     public void verifyEmployeesTest() throws InterruptedException {
         dashboardPage.navigateToPIM();
-        Assert.assertTrue(pimPage.verifyEmployee("sai ss"));
+        Assert.assertTrue(pimPage.verifyEmployee("Sai"));
         dashboardPage.navigateToPIM();
-        Assert.assertTrue(pimPage.verifyEmployee("shailu sm"));
+        Assert.assertTrue(pimPage.verifyEmployee("shailu"));
         dashboardPage.navigateToPIM();
-        Assert.assertTrue(pimPage.verifyEmployee("Bhanu kk"));
+        Assert.assertTrue(pimPage.verifyEmployee("Bhanu"));
         dashboardPage.navigateToPIM();
-        Assert.assertTrue(pimPage.verifyEmployee("shravs ll"));
+        Assert.assertTrue(pimPage.verifyEmployee("shravs"));
+    }
+    
+    @Test(priority = 4)
+    public void logoutTest() {
+    	Actions act = new Actions(driver);
+  	  act.moveToElement(driver.findElement(By.className("oxd-userdropdown-tab"))).build().perform();
+       driver.findElement(By.linkText("Logout")).click();
+       
     }
 
     
